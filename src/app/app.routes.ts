19a939fdbfe1171d37b954643b1bbf4ea05297bb
reinisa <reinis411@gmail.com>
2025-02-05 22:39:01 +0200

    Added home component and styles for table

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pieprasijums/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"pieprasijums/home", component:HomeComponent},
+    {path:"pieprasijums", redirectTo:"pieprasijums/home", pathMatch:"full"},
+    {path:"", redirectTo:"pieprasijums/home", pathMatch:"full"}
+];
